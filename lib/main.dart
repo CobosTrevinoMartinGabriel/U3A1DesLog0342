9c6396a99065545e2e3e6c52a31addbe34698077
Martin_Gabriel_Cobos_Trevino <a.21308051280342@cbtis128.edu.mx>
2024-06-03 15:11:17 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,167 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // Application name
       title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
+      debugShowCheckedModeBanner: false,
+      // Application theme data, you ca
+      // n set the colors for the application as
       // you want
       theme: ThemeData(
         // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Loged(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Loged extends StatefulWidget {
+  const Loged({Key? key}) : super(key: key);
+
+  @override
+  State<Loged> createState() => _LogedState();
+}
+
+class _LogedState extends State<Loged> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Color(0xfffe9410),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Image.network(
+                "https://raw.githubusercontent.com/CobosTrevinoMartinGabriel/Img_ios/main/Capcom-logo.png"),
+          )
+        ],
+        title: Text(
+          "Login",
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    fillColor: Colors.blue,
+                    prefixIcon: Icon(Icons.mail),
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.password),
+                      border: OutlineInputBorder(),
+                      labelText: "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
+                child: Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff97bdff),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        if (emailController.text == "corexmartex@gmail.com" &&
+                            passwordController.text == "325423") {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    HomePage(email: emailController.text),
+                              ));
+                          passwordController.text = "";
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content:
+                                      Text("Contraseña o correo invalidos")));
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                    child: const Text('Iniciar sesion'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Image.network(
+                    "https://raw.githubusercontent.com/CobosTrevinoMartinGabriel/Img_ios/main/Ryu-SSFII-intro-ezgif.com-webp-to-gif-converter.gif"),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.email});
+
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff1897ff),
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: const Text('Bienvenido de vuelta, jugador'),
+        ),
+        body: Column(
+          children: [
+            Text(email),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cerrar Sesion!"),
+              ),
+            ),
+          ],
+        ));
   }
 }
